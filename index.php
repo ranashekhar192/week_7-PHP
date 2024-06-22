061d91dc2c7f49047d2af08918b3aeaa287bc8bd
ranashekhar192 <ranashekhar192001@gmail.com>
2024-06-22 04:10:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- /index.php -->
+<?php include 'includes/header.php'; ?>
+<main>
+<h1>THE FAMOUS ACTOR</h1>
+<p>Salman Khan (born December 27, 1965, Indore, Madhya Pradesh, India) is a Bollywood actor, dancer, film producer, and television presenter. Along with actors Shah Rukh Khan and Aamir Khan, Salman Khan is one of the three most popular Khans of Bollywood.</p>
+<img src="salman khan.jpeg" alt="SALMAN KHAN">
+</main>
+<?php include 'includes/footer.php'; ?>
